b7977f1ccba0306a1b8c187a5166d7e6d39128f2
norawolf <noraevogt@gmail.com>
2019-01-25 13:57:32 -0500

    Initialize repo

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source "https://rubygems.org"
+
+# Specify your gem's dependencies in notable_books_2018.gemspec
+gemspec
---
+++ lib/environment.rb
@@ -0,0 +1,3 @@
+require "notable_books_2018/version"
+
+# here we will require all relevant files in dir notable_books_2018
