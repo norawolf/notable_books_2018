d745e754ab0e2323935cf5164bd9b74d1b5f288d
norawolf <noraevogt@gmail.com>
2019-01-26 16:39:50 -0500

    pseudocode for CLI

--- lib/notable_books_2018/cli.rb
+++ lib/notable_books_2018/cli.rb
@@ -1,32 +1,90 @@
 class NotableBooks2018::CLI
   def start
-    puts "NYT's Notable Books of 2018"
+    welcome
+    main_menu
+    book_selector
+    goodbye
+  end
+
+  def welcome
+    puts "Welcome to NYT's Notable Books of 2018."
+    puts "Project description here."
+  end
+
+  def main_menu
+    puts <<~DOC
+
+      Enter the number of books you would like to view.
+      1-19
+      20-39
+      40-59
+      60-79
+      80-100
+    DOC
+  end
+
+  def book_selector
+    input = nil
+
+    while input != "exit"
+      #initial input to choose numbered list of books
+      input = gets.chomp
+      puts "Which book would you like more information about?"
+
+
+      print_book_list(input)
+
+      #How to make it so that user could enter "exit" even when not prompted and still exit?
+      input = gets.chomp
+
+      # method below will become something to find the index of the book
+      # book_index = NotableBooks2018::Books.find(input.to_i)
+
+      print_book_info(input)
+
+      puts "Would you like to see another book? Enter 'Yes' or 'No'."
+
+      input = gets.chomp.downcase
+
+      case input
+        when "yes"
+          # should this go to the main menu, or back to the smaller list? if so, how?
+          main_menu
+          book_selector
+        when "no"
+          goodbye
+         else
+          puts "I don't understand that answer."
+          main_menu
+          book_selector
+      end
+
+    end
+  end
+
+  def print_book_list(by_number)
+      puts "1. Title by Author"
+      puts "2. Title by Author"
+      puts "3. Title by Author"
+      puts "4. Title by Author"
+      puts "5. Title by Author"
+  end
+
+
+  def print_book_info(book_index)
+    puts <<-HEREDOC
+    Book cover URL
+    Book author
+    Book Price
+    Book Publisher
+    Book description
+    HEREDOC
+  end
+
+
+  def goodbye
+    puts "Thank you for browsing. Goodbye!"
+    exit
   end
 
 end
-
-# Welcome message, project description.
-# How would you like to display the books?
-
-# List all books
-  # List all 100 books
-  # Choose which book you would like more info on.
-    # next level of all book info + descriptions
-# Search authors
-# Seach books by genre
-
-# OR
-
-# ask user how many books they would like to see: 10? >> in the CLI
-# main screen loads all books
-  # also can include link to book cover
-  # or use a gim to pixelate cover in the terminal
-# user can choose a book and then see more description
-# build scraper first
-# then book class
-# then CLI (formatting happens in CLI)
-
-# to avoid over-scraping
-# use ||=
-# set scraper to instance variable, indicate that until we exit program, keep using this variable
-# or set it equal to a new scrape if scrape has not already occurred
