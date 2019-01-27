d9c234e4d4ee53a846d95b4169ea71bce7f92bd6
norawolf <noraevogt@gmail.com>
2019-01-27 11:49:43 -0500

    add book info print functionality to #print_book_info

--- lib/notable_books_2018/cli.rb
+++ lib/notable_books_2018/cli.rb
@@ -13,9 +13,12 @@ class NotableBooks2018::CLI
   end
 
   def main_menu
+    # How to phrase this?
     puts <<~DOC
 
-      Enter the number of books you would like to view.
+      Please enter a number to see a list of books.
+
+      Books:
       1-10
       11-19
       20-29
@@ -35,16 +38,12 @@ class NotableBooks2018::CLI
     while input != "exit"
       #initial input to choose numbered list of books
       input = gets.chomp.to_i
-      puts "Enter a book number you would like more information about?"
-
 
       print_book_list(input)
 
+      puts "Enter the number of a book you would like more information about."
       #How to make it so that user could enter "exit" even when not prompted and still exit?
-      input = gets.chomp
-
-      # method below will become something to find the index of the book
-      # book_index = NotableBooks2018::Books.find(input.to_i)
+      input = gets.chomp.to_i
 
       print_book_info(input)
 
@@ -60,7 +59,7 @@ class NotableBooks2018::CLI
         when "no"
           goodbye
          else
-          puts "I don't understand that answer."
+          puts "That is not a valid selection."
           main_menu
           book_selector
       end
@@ -85,14 +84,21 @@ class NotableBooks2018::CLI
 
 
   def print_book_info(book_index)
-    #NotableBooks2018.Book.information
-    puts <<-HEREDOC
-    Book cover URL
-    Book author
-    Book Price
-    Book Publisher
-    Book description
-    HEREDOC
+    NotableBooks2018::Book.all.each.with_index(1) do |book, index|
+      if index == book_index
+        puts book.title
+        puts "by #{book.author}"
+        puts ""
+        puts "Genre(s): #{book.genre}"
+        puts ""
+        puts "Description: #{book.description}"
+        puts ""
+        #How to display book description with auto line wrapping? Or is it just my terminal
+        # and small screen?
+        # formatting - lines?
+        # colorizing?
+      end
+    end
   end
 
 
