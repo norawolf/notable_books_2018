3368062b245d44cf829dd334e1e19f6aa1dc2258
norawolf <noraevogt@gmail.com>
2019-01-26 23:02:54 -0500

    comment out genre scraping due to parsing error

--- lib/notable_books_2018/scraper.rb
+++ lib/notable_books_2018/scraper.rb
@@ -5,22 +5,25 @@ class NotableBooks2018::Scraper
     Nokogiri::HTML(open("https://www.nytimes.com/interactive/2018/11/19/books/review/100-notable-books.html"))
   end
 
+
   def self.scrape_book_info
-    all_books = []
+    # like the student scraper lab
+    book_array = []
      scrape_page.css(".g-book-data").each do |nodeset|
        book_hash = {}
        book_hash[:title] = nodeset.css(".g-book-title").text.strip
        book_hash[:author] = nodeset.css(".g-book-author b").text.strip
-       book_hash[:genre] = nodeset.css("span.g-book-tag").text.strip
+       #book_hash[:genre] = nodeset.css(".g-book-tag").text.strip
+       binding.pry
        book_hash[:description] = nodeset.css(".g-book-description").text.strip
-      all_books << book_hash
+      book_array << book_hash
       end
-      all_books
-  end
+      book_array
+end
 
   # def self.scrape_more_info
       # expanded functionality to scrape for
       # :cover_url
-      # and scrape + parse :publisher and :price
+      # and scrape + parse :publisher and :price and :review_url
   # end
 end
