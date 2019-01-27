57c06bb4da6fdcb87081528c50d158d986a027bf
norawolf <noraevogt@gmail.com>
2019-01-26 21:43:39 -0500

    complete book main  data scraper

--- lib/notable_books_2018/scraper.rb
+++ lib/notable_books_2018/scraper.rb
@@ -1,20 +1,26 @@
-#Can't get the NotableBooks2018::Scraper namespacing to work. Gives uninitialized constant NameError
 require 'pry'
 
 class NotableBooks2018::Scraper
   def self.scrape_page
-    page = Nokogiri::HTML(open("https://www.nytimes.com/interactive/2018/11/19/books/review/100-notable-books.html"))
+    Nokogiri::HTML(open("https://www.nytimes.com/interactive/2018/11/19/books/review/100-notable-books.html"))
   end
 
   def self.scrape_book_info
-    scrape_page.css(".g-books")
+    all_books = []
+     scrape_page.css(".g-book-data").each do |nodeset|
+       book_hash = {}
+       book_hash[:title] = nodeset.css(".g-book-title").text.strip
+       book_hash[:author] = nodeset.css(".g-book-author b").text.strip
+       book_hash[:genre] = nodeset.css("span.g-book-tag").text.strip
+       book_hash[:description] = nodeset.css(".g-book-description").text.strip
+      all_books << book_hash
+      end
+      all_books
   end
 
-  def self.create_books
-     scrape_book_info.each do |nodeset|
-       binding.pry
-        #Book.new_from_page(nodeset)
-        #nodeset is now all the data for the books
-      end
-  end
+  # def self.scrape_more_info
+      # expanded functionality to scrape for
+      # :cover_url
+      # and scrape + parse :publisher and :price
+  # end
 end
