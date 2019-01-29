638f5e68c9902e6ac46e891aafaeb609f3294d80
norawolf <noraevogt@gmail.com>
2019-01-29 13:03:09 -0500

    refactor genre_parse method to use Genre.find_or_create_by_name

--- lib/notable_books_2018/scraper.rb
+++ lib/notable_books_2018/scraper.rb
@@ -7,10 +7,14 @@ class NotableBooks2018::Scraper
 
   def self.genre_parse(css)
     genre_data = css
-    
-    genre_data.collect do |genre_name|
-      NotableBooks2018::Genre.new(genre_name)
-    end
+
+    genre_data.collect do |genre_name| #this is the (1-3) strings inside of the hash[:genre] array
+      #create a new genre or return the genre if it already exists
+      #how to find if a Genre already exists?
+
+      NotableBooks2018::Genre.find_or_create_by_name(genre_name)
+
+     end
   end
 
   def self.scrape_book_info
@@ -23,15 +27,7 @@ class NotableBooks2018::Scraper
        book_hash[:description] = nodeset.css(".g-book-description").text.strip
       books_array << book_hash
       end
+
       NotableBooks2018::Book.create_from_collection(books_array)
   end
-
-  #could create a helper method to correctly parse & format genres
-  # currently, genre is a string containing 1-3 values
-  # would like for a Book to have one or multiple Genre instances
-
-
-   #then
-    #book_hash[:genre] = genre_parse
-  # end
 end
