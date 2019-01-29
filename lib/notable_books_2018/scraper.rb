93be577c3c3deca44065fea2be4116a4de7678f1
norawolf <noraevogt@gmail.com>
2019-01-29 09:23:45 -0500

    set genre attribute to new instance of Genre class

--- lib/notable_books_2018/scraper.rb
+++ lib/notable_books_2018/scraper.rb
@@ -12,10 +12,12 @@ class NotableBooks2018::Scraper
        book_hash = {}
        book_hash[:title] = nodeset.css(".g-book-title").text.strip
        book_hash[:author] = nodeset.css(".g-book-author b").text.strip.chomp(".")
-       book_hash[:genre] = nodeset.css(".g-book-tag").text.split.join(' ')
+       book_hash[:genre] = NotableBooks2018::Genre.new(nodeset.css(".g-book-tag").text.split.join(' '))
+       binding.pry
        book_hash[:description] = nodeset.css(".g-book-description").text.strip
       books_array << book_hash
       end
       NotableBooks2018::Book.create_from_collection(books_array)
   end
+
 end
