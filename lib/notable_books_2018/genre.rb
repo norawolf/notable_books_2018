4e90e17615be5de1ccfa918539af8b67df990fbd
norawolf <noraevogt@gmail.com>
2019-01-29 12:59:39 -0500

    add find_or_create_by methods to Genre class

--- lib/notable_books_2018/genre.rb
+++ lib/notable_books_2018/genre.rb
@@ -2,8 +2,35 @@ class NotableBooks2018::Genre
 
   attr_accessor :name
 
+  @@all = []
+
   def initialize(name)
     @name = name
+    @@all << self
   end
-  
+
+  def self.all
+    @@all
+  end
+
+  def self.find_by_name(name)
+    all.find {|genre| genre.name == name}
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.create_by_name(name)
+    NotableBooks2018::Genre.new(name).tap {|genre| genre.save}
+  end
+
+  def self.find_or_create_by_name(name)
+    find_by_name(name) || create_by_name(name)
+  end
+
+  # def books
+  #   NotableBooks2018::Book.all.select {|book| book.genre == self}
+  # end
+
 end
