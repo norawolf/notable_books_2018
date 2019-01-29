aceab3f875627a0aea3c8a8421eeccb73039caa5
norawolf <noraevogt@gmail.com>
2019-01-29 11:06:32 -0500

    instantiate new Genres for books with multiple genres

--- lib/notable_books_2018/scraper.rb
+++ lib/notable_books_2018/scraper.rb
@@ -5,6 +5,25 @@ class NotableBooks2018::Scraper
     Nokogiri::HTML(open("https://www.nytimes.com/interactive/2018/11/19/books/review/100-notable-books.html"))
   end
 
+  def self.genre_parse(css)
+    genre_data = css
+
+    #refactor later
+    if genre_data.size == 1
+      NotableBooks2018::Genre.new(genre_data[0])
+    elsif genre_data.size == 2
+      array = []
+      array << NotableBooks2018::Genre.new(genre_data[0])
+      array << NotableBooks2018::Genre.new(genre_data[1])
+      array
+    elsif genre_data.size == 3
+      array = []
+     array << NotableBooks2018::Genre.new(genre_data[0])
+     array << NotableBooks2018::Genre.new(genre_data[1])
+     array << NotableBooks2018::Genre.new(genre_data[2])
+     array
+    end
+  end
 
   def self.scrape_book_info
     books_array = []
@@ -12,8 +31,7 @@ class NotableBooks2018::Scraper
        book_hash = {}
        book_hash[:title] = nodeset.css(".g-book-title").text.strip
        book_hash[:author] = nodeset.css(".g-book-author b").text.strip.chomp(".")
-       book_hash[:genre] = NotableBooks2018::Genre.new(nodeset.css(".g-book-tag").text.split.join(' '))
-       binding.pry
+       book_hash[:genre] = genre_parse(nodeset.css(".g-book-tag").text.split)
        book_hash[:description] = nodeset.css(".g-book-description").text.strip
       books_array << book_hash
       end
@@ -24,17 +42,8 @@ class NotableBooks2018::Scraper
   # currently, genre is a string containing 1-3 values
   # would like for a Book to have one or multiple Genre instances
 
-  # def genre_parse
-    #creates an array of string data, removes whitespace
-    #genre_data = nodeset.css(".g-book-tag").text.split
 
-    #if genre_data.size = 1
-      # create one new Genre, assign it to book_hash[:genre]
-    #if genre_data.size = 2
-      # above x2
-    #if genre_data.size = 3
-     # above x3
-   #end
-
+   #then
+    #book_hash[:genre] = genre_parse
   # end
 end
