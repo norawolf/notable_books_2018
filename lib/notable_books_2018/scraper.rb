25e27744dbbee10e343ed37276b067d12204360e
norawolf <noraevogt@gmail.com>
2019-01-30 21:43:38 -0500

    reorder methods; delete comments

--- lib/notable_books_2018/scraper.rb
+++ lib/notable_books_2018/scraper.rb
@@ -2,17 +2,8 @@ require 'pry'
 
 class NotableBooks2018::Scraper
   def self.scrape_page
-    Nokogiri::HTML(open("https://www.nytimes.com/interactive/2018/11/19/books/review/100-notable-books.html"))
-  end
-
-  def self.create_genres(css)
-    genre_data = css
-
-    genre_data.collect do |genre_name| #this is the (1-3) strings inside of the hash[:genre] array
-      #create a new genre or return the genre if it already exists
-
-      NotableBooks2018::Genre.find_or_create_by_name(genre_name)
-     end
+    url = "https://www.nytimes.com/interactive/2018/11/19/books/review/100-notable-books.html"
+    Nokogiri::HTML(open(url))
   end
 
   def self.scrape_book_info
@@ -21,11 +12,19 @@ class NotableBooks2018::Scraper
        book_hash = {}
        book_hash[:title] = nodeset.css(".g-book-title").text.strip
        book_hash[:author] = nodeset.css(".g-book-author b").text.strip.chomp(".")
-       book_hash[:genre] = create_genres(nodeset.css(".g-book-tag").text.split(".").map!(&:strip).reject(&:empty?))
+       book_hash[:genre] = create_genres(nodeset.css(".g-book-tag").text
+        .split(".").map!(&:strip).reject(&:empty?))
        book_hash[:description] = nodeset.css(".g-book-description").text.strip
       books_array << book_hash
       end
 
       NotableBooks2018::Book.create_from_collection(books_array)
+  end
+
+  def self.create_genres(css)
+    genre_data = css
+    genre_data.collect do |genre_name|
+      NotableBooks2018::Genre.find_or_create_by_name(genre_name)
+     end
   end
 end
