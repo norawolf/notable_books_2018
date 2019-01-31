c376e90f0e8da8debb5aef0af2451ce78eac071a
norawolf <noraevogt@gmail.com>
2019-01-31 11:35:29 -0500

    create helper method to parse book publication info

--- lib/notable_books_2018/scraper.rb
+++ lib/notable_books_2018/scraper.rb
@@ -10,15 +10,24 @@ class NotableBooks2018::Scraper
     books_array = []
      scrape_page.css(".g-book-data").each.with_index do |nodeset|
        book_hash = {}
+
        book_hash[:title] = nodeset.css(".g-book-title").text.strip
        book_hash[:author] = nodeset.css(".g-book-author b").text.strip.chomp(".")
        book_hash[:genre] = create_genres(nodeset.css(".g-book-tag").text
         .split(".").map!(&:strip).reject(&:empty?))
        book_hash[:description] = nodeset.css(".g-book-description").text.strip
+       book_hash[:publication_info] = parse_publication_info(nodeset.css(".g-book-author"))
+       binding.pry
       books_array << book_hash
       end
 
       NotableBooks2018::Book.create_from_collection(books_array)
+  end
+
+  def self.parse_publication_info(css)
+    details_array = css.text.split(". ").map!(&:strip)
+    details_array.delete_at(0)
+    details_array.join(". ")
   end
 
   def self.create_genres(css)
